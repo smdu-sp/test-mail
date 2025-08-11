08a2ebc39c3baadada656b8214897a234623d5ae
Victor Abreu <aalevictor@gmail.com>
2025-08-11 14:30:55 -0300

    logo

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
-import cmpu from "../public/logo-final-eleicao-CMPU_300x200.png"
-import logo from "../public/logo-urban.png"
 import { FileDown } from "lucide-react";
 
 export default function CMPUForm() {
@@ -12,8 +10,8 @@ export default function CMPUForm() {
     <div className="flex flex-col min-h-screen bg-gray-100 md:px-18">
       <div className="w-full max-w-[1500px] mx-auto">
         <div className="flex justify-between items-center p-6">
-          <Image className="w-40" src={cmpu} alt={"logo cmpu"} />
-          <Image className="w-40" src={logo} alt={"logo sp"} />
+          <Image className="w-40" width={160} height={100} src="/cmpu_logo.png" alt={"logo cmpu"} />
+          <Image className="w-40" width={160} height={100} src="/prefeitura_logo.png" alt={"logo sp"} />
         </div>
         <div className="w-full">
           <div className="w-[96%] border-b-4 border-black mt-2 mx-auto mb-4"></div>
